18e67b73f0938cd9f229ec3e0ac816f1836f9825
EhtishamNazir <41062866+EhtishamNazir@users.noreply.github.com>
2021-12-08 11:53:19 +0500

    add feature what day is today

--- app.js
+++ app.js
@@ -3,15 +3,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
-	var today = new Date();
-	var currentDay = today.getDay();
+app.set('view engine', 'ejs')
 
-	if (currentDay === 6 || currentDay === 0) {
-		res.send("Yay It's weekend!");
-	} else{
-		res.send("Boo! I have to work today");
+app.get("/", (req, res) => {
+	let today = new Date();
+	let currentDay = today.getDay();
+	let day = "";
+
+	switch(currentDay){
+		case 0:
+		 day = "Sunday";
+		 break;
+		case 1:
+		 day = "Monday";
+		 break;
+		case 2:
+		 day = "Tuesday";
+		 break;
+		case 3:
+		 day = "Wednesday";
+		 break;
+		case 4:
+		 day = "Thursday";
+		 break;
+		case 5:
+		 day = "Friday";
+		 break;
+		case 6:
+		 day = "Saturday";
+		 break;
+		default:
+		 console.log("Error: current day is equal to: " + currentDay);
+
 	}
+
+	res.render("list", {kindOfDay: day});
 	
 });
 
